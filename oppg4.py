27231cf6b9294c8d6bb0fc46419010e59249f1be
mhblystad <magnushbl@gmail.com>
2024-10-17 21:01:43 +0200

    Beta

---
+++ oppg4.py
@@ -0,0 +1,95 @@
+
+# data = {
+#     "Norge": ["Oslo", 0.634],
+#     "England": ["London", 8.982],
+#     "Frankrike": ["Paris", 2.161],
+#     "Italia": ["Roma", 2.873]
+# } <--Denne malen skrev jeg inn først her, og siden matet jeg den til chatgtp for å få en liste som inneholdt alle land i europa for de gadd jeg ikke skrive ut for hånd
+print(r" /$$$$$$$$ /$$   /$$ /$$$$$$$   /$$$$$$  /$$$$$$$   /$$$$$$                                                        ")
+print(r"| $$_____/| $$  | $$| $$__  $$ /$$__  $$| $$__  $$ /$$__  $$                                                       ")
+print(r"| $$      | $$  | $$| $$  \ $$| $$  \ $$| $$  \ $$| $$  \ $$                                                       ")
+print(r"| $$$$$   | $$  | $$| $$$$$$$/| $$  | $$| $$$$$$$/| $$$$$$$$                                                       ")
+print(r"| $$__/   | $$  | $$| $$__  $$| $$  | $$| $$____/ | $$__  $$                                                       ")
+print(r"| $$      | $$  | $$| $$  \ $$| $$  | $$| $$      | $$  | $$                                                       ")
+print(r"| $$$$$$$$|  $$$$$$/| $$  | $$|  $$$$$$/| $$      | $$  | $$                                                       ")
+print(r"|________/ \______/ |__/  |__/ \______/ |__/      |__/  |__/                                                       ")
+print(r"                                                                                                                   ")
+print(r"                                                                                                                   ")
+print(r"                                                                                                                   ")
+print(r" /$$   /$$                                     /$$             /$$                     /$$                         ")
+print(r"| $$  | $$                                    | $$            | $$                    | $$                         ")
+print(r"| $$  | $$  /$$$$$$  /$$    /$$ /$$$$$$   /$$$$$$$  /$$$$$$$ /$$$$$$    /$$$$$$   /$$$$$$$  /$$$$$$   /$$$$$$       ")
+print(r"| $$$$$$$$ /$$__  $$|  $$  /$$//$$__  $$ /$$__  $$ /$$_____/|_  $$_/   |____  $$ /$$__  $$ /$$__  $$ /$$__  $$      ")
+print(r"| $$__  $$| $$  \ $$ \  $$/$$/| $$$$$$$$| $$  | $$|  $$$$$$   | $$      /$$$$$$$| $$  | $$| $$$$$$$$| $$  \__/      ")
+print(r"| $$  | $$| $$  | $$  \  $$$/ | $$_____/| $$  | $$ \____  $$  | $$ /$$ /$$__  $$| $$  | $$| $$_____/| $$            ")
+print(r"| $$  | $$|  $$$$$$/   \  $/  |  $$$$$$$|  $$$$$$$ /$$$$$$$/  |  $$$$/|  $$$$$$$|  $$$$$$$|  $$$$$$$| $$            ")
+print(r"|__/  |__/ \______/     \_/    \_______/ \_______/|_______/    \___/   \_______/ \_______/ \_______/|__/            ")
+print(r"                                                                                                                   ")
+print(r"                                                                                                                   ")
+print(r"                                                                                                                   ")
+print(r"                            /$$$$$$          /$$ /$$                                                               ")
+print(r"                           /$$__  $$        | $$| $$                                                               ")
+print(r"  /$$$$$$   /$$$$$$       | $$  \__//$$$$$$ | $$| $$   /$$                                                         ")
+print(r" /$$__  $$ /$$__  $$      | $$$$   /$$__  $$| $$| $$  /$$/                                                         ")
+print(r"| $$  \ $$| $$  \ $$      | $$_/  | $$  \ $$| $$| $$$$$$/                                                          ")
+print(r"| $$  | $$| $$  | $$      | $$    | $$  | $$| $$| $$_  $$                                                          ")
+print(r"|  $$$$$$/|  $$$$$$$      | $$    |  $$$$$$/| $$| $$ \  $$                                                         ")
+print(r" \______/  \____  $$      |__/     \______/ |__/|__/  \__/                                                         ")
+print(r"           /$$  \ $$                                                                                               ")
+print(r"          |  $$$$$$/                                                                                               ")
+print(r"           \______/                                                                                                 ")
+
+data = {
+    "Norge": ["Oslo", 0.634],
+    "England": ["London", 8.982],
+    "Frankrike": ["Paris", 2.161],
+    "Italia": ["Roma", 2.873],
+    "Tyskland": ["Berlin", 3.769],
+    "Spania": ["Madrid", 3.334],
+    "Sverige": ["Stockholm", 0.975],
+    "Danmark": ["København", 0.799],
+    "Finland": ["Helsinki", 0.631],
+    "Polen": ["Warszawa", 1.795],
+    "Nederland": ["Amsterdam", 0.872],
+    "Belgia": ["Brussel", 1.222],
+    "Østerrike": ["Wien", 1.931],
+    "Sveits": ["Bern", 0.133],
+    "Portugal": ["Lisboa", 0.545],
+    "Hellas": ["Athen", 3.153],
+    "Ungarn": ["Budapest", 1.752],
+    "Tsjekkia": ["Praha", 1.324],
+    "Irland": ["Dublin", 1.228],
+    "Island": ["Reykjavik", 0.131],
+    "Slovakia": ["Bratislava", 0.437],
+    "Slovenia": ["Ljubljana", 0.293],
+    "Kroatia": ["Zagreb", 0.809],
+    "Serbia": ["Beograd", 1.680],
+    "Bosnia-Hercegovina": ["Sarajevo", 0.275],
+    "Montenegro": ["Podgorica", 0.152],
+    "Albania": ["Tirana", 0.508],
+    "Nord-Makedonia": ["Skopje", 0.544],
+    "Kosovo": ["Pristina", 0.211],
+    "Bulgaria": ["Sofia", 1.249],
+    "Romania": ["Bucuresti", 1.830],
+    "Litauen": ["Vilnius", 0.588],
+    "Latvia": ["Riga", 0.632],
+    "Estland": ["Tallinn", 0.437],
+    "Hviterussland": ["Minsk", 2.020],
+    "Ukraina": ["Kyiv", 2.884],
+    "Russland": ["Moskva", 12.400],  # European part of Russia
+    "Moldova": ["Chisinau", 0.492],
+    "Luxembourg": ["Luxembourg", 0.128],
+    "Monaco": ["Monaco", 0.039],
+    "Liechtenstein": ["Vaduz", 0.005],
+    "Andorra": ["Andorra la Vella", 0.022],
+    "San Marino": ["San Marino", 0.034],
+    "Malta": ["Valletta", 0.006],
+    "Vatikanstaten": ["Vatikanstaten", 0.001]
+}
+land = ""
+while land not in data:
+    land = input("Skriv inn et land i Europa, også skal du få vite hovedstad, og ca hvor mange millioner som bor i hovedstaden!")
+    try:
+        print(land, "er landet du valgte, og hovedstaden er ", data[land][0], "og der bor det ca", data[land][1],"millioner mennesker. WOW! Hvem skulle trodd det? Andre de som bor der da.")
+    except:
+        ValueError("Du har ikke skrevet et land i Europa. Du klarer vel å komme på et?")
